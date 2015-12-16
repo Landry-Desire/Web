3e578781f37d17ede675c2b5100082cd20b28cd8
euskanani <euskanani@localhost.localdomain>
2015-12-16 12:29:33 +0100

    view init

---
+++ views/routes.js
@@ -0,0 +1,21 @@
+var app = angular.module('myApp',['ui.router'])
+
+//using ui-router
+app.config(function($stateProvider, $urlRouterProvider){   
+	// For any unmatched url, send to /route1
+	$urlRouterProvider.otherwise("/dashboard")
+
+	$stateProvider
+	.state('connexion', {
+		url: "/",
+		templateUrl: "index.html",
+		controller: 'connexionCtrl',
+	})
+
+
+	.state('dashboard', {
+		url: "/dashboard",
+		templateUrl: "partials/dashboard.html",
+		controller: 'dashboardCtrl',
+	})
+
