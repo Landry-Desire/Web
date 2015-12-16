c3d4ade54363fae647bd8aa390e383f0f1209664
euskanani <euskanani@localhost.localdomain>
2015-12-16 13:17:54 +0100

    bootstrap url

--- views/js/connexion.js
+++ views/js/connexion.js
@@ -1,10 +1,8 @@
 var app=angular.module('myApp', [])
 
-
-
 app.controller("connexionCtrl",['$scope','$http','$location', function($scope,$http,$location) {
 
-	$scope.submitLogin = function() {
+	$scope.submitLogin= function() {
 		var params =JSON.stringify( { email: $scope.pseudo, password :$scope.password})
 		alert($scope.pseudo,$scope.password)
 		$http.post('http://localhost:3000/users/login',params)
@@ -13,7 +11,7 @@ app.controller("connexionCtrl",['$scope','$http','$location', function($scope,$h
                 $scope.userloggedin = user;
 				$location.url('/dashboard');
 			}
-		})
+		)
 		.error(function(status) {
 			console.log(status);
 		});
