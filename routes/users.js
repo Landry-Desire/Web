0b5f350e81d7e58b9140724ff054f864153af4d8
nureynisow <nureynisow@gmail.com>
2015-12-18 16:10:36 +0100

    Signin Login Logout API DONE

--- routes/users.js
+++ routes/users.js
@@ -4,67 +4,89 @@ var router = express.Router();
 
 var User = require('./userModel').User;
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send({'SESSION_VAR':req.session});
 });
 
 router.get('/login',function (req, res, next) {
-  console.log(req.session.pseudo); 
 	res.render('login');
 });
 
+router.get('/signup',function (req, res, next) {
+  res.render('signup');
+});
 
 router.post('/signup',function (req, res, next) {
 	var userBody =  req.body
 	var u = new User();
 	User.findOne({'pseudo':userBody.pseudo},function (err,user) {
 		if(err)
-			console.log(err);
+			res.send({
+        'message':err,
+        'success':false
+      });
 		if(user){
-			res.send({'error':'user already in bd'});
+			res.send({
+        'message':'user already exists',
+        'success':false
+      });
 		}else{
 			u.pseudo  = userBody.pseudo;
 			u.password = userBody.password;
 			u.save(function (err) {
 				if (err)
-					console.error(err);
-				console.log("signed up ",u.pseudo);
-				res.redirect('/users/login');
+					res.send({
+            'message':err,
+            'success':false
+          });
+				res.send({
+          'message':'A user with <i>pseudo : <b>'+u.pseudo+'</b></i> created. Please login !',
+          'success':true
+        });
 			});		
 		}
-
 	});
-	
 });
 
 router.post('/login',function(req,res,next){
   var users = req.body;
   User.findOne({'pseudo':users.pseudo},function(err,user){
     if(err)
-      console.log(err);
+      res.send({
+        'message':err,
+        'success':false
+      });
     if(user){
         console.log(user)
         req.session.pseudo = user.pseudo;
         console.log(req.session.pseudo)
         if(user.pseudo==users.pseudo && user.password==users.password){
-          res.send({'success':'ok'})
-          //res.require('/')
+          res.send({
+            'message':'User <i> '+user.pseudo+' </i> authenticated !',
+            'success':true
+          });
         }else if(user.pseudo==users.pseudo && user.password!=users.password)
         {
-          res.send({'error':'User or password not found'})
+          res.send({
+            'message':'user not found',
+            'success':false
+          });
         }
     }else{
-      res.send({'error':'bad user!'})
+      res.send({
+        'message':'User not found',
+        'success':false
+      });
     }
   });
 });
 
 router.get('/logout', function (req, res) {
    delete req.session.pseudo;
-   console.log(req.session.pseudo);
-   res.send("deconnection"+req.session.pseudo);
-   //res.redirect('/login');
+   res.send({
+        'message':'User Logged Out',
+        'success':true
+      });
 }); 
 
 module.exports = router;
